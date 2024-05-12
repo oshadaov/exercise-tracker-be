5aa9547cd89b0e2fe6c93cf2c576b961fafca720
oshadaov <119332246+oshadaov@users.noreply.github.com>
2024-05-12 22:24:31 +0530

    created models

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose')
 
 require('dotenv').config();
 
@@ -9,6 +9,23 @@ const port = process.env.PORT || 5005;
 
 app.use(cors());
 app.use(express.json());
+
+const mongoURI = "mongodb+srv://oshadaET:mean123@exercisetrackerdb.yewxxkq.mongodb.net/?retryWrites=true&w=majority&appName=exerciseTrackerDB;"; // Replace with your connection details
+
+
+
+mongoose.connect(mongoURI, {
+  // Other connection options (e.g., useNewUrlParser, useUnifiedTopology)
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
+
+
+const exerciseRouter = require('./routes/exercise');
+const userRouter = require('./routes/user')
+
+app.use('/exercises',exerciseRouter);
+app.use('/users',userRouter)
 
 app.listen(port, ()=> {
 
